f2197e20cd88bc72168ab42bd20b82fe094a5934
DomScripts <drogucki02@gmail.com>
2025-12-02 11:15:24 -0500

    Split the SQL table setup and loading of the static data into 2 files.

--- sql/setup.sql
+++ sql/setup.sql
@@ -36,14 +36,3 @@ CREATE TABLE IF NOT EXISTS Portfolio (
     UNIQUE(AssetID),
     FOREIGN KEY (AssetID) REFERENCES Asset(AssetID)
 );
-
--- Preloaded data
-INSERT INTO Asset (AssetID, Symbol, Name, AssetType, Exchange) VALUES
-(1, 'BTCUSDT', 'Bitcoin', 'Crypto', 'BinanceUS'),
-(2, 'ETHUSDT', 'Ethereum', 'Crypto', 'BinanceUS'),
-(3, 'SOLUSDT', 'Solana', 'Crypto', 'BinanceUS');
-
-INSERT INTO Portfolio (PortfolioID, AssetID, QuantityHeld, AverageCost) VALUES
-(1, 1, 10.0, 86777.81),
-(2, 2, 150.0, 2806.95),
-(3, 3, 400.0, 127.84);
